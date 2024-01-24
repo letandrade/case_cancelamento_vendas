ac9d89695c9068a6d474e5c43b25ce946e5ba364
Letícia Andrade <86376728+letandrade@users.noreply.github.com>
2024-01-24 19:12:59 -0300

    Update fast_api.py

--- fast_api.py
+++ fast_api.py
@@ -39,7 +39,7 @@ modelo = carregar_modelo(caminho_do_modelo)
 
 @app.post("/fazer-predicao")
 def operate(input_data:User_input):
-    resultado = fazer_predicao(modelo, [input_data.DiaSemana, input_data.FaixaHora, input.Mes, input_data.ClienteID, input_data.QuantidadeItens,
+    resultado = fazer_predicao(modelo, [input_data.DiaSemana, input_data.FaixaHora, input_data.Mes, input_data.ClienteID, input_data.QuantidadeItens,
                                     input_data.Vendas, input_data.MediaPreco, input_data.QtdItensDistintos, input_data.QtdProduto1,
                                     input_data.QtdProduto2, input_data.QtdProduto3, input_data.QtdProduto4, input_data.QtdProduto5, input_data.QtdProduto6,
                                     input_data.QtdProduto7, input_data.QtdProduto8, input_data.QtdProduto9, input_data.QtdProduto10, input_data.QtdProduto11,
